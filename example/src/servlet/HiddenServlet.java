802cd3752c37cac5f42c412a43cec746f20a04a0
SeiichiN <billie175@gmail.com>
2022-12-19 16:28:24 +0900

    次は練習5.6から

--- example/src/servlet/HiddenServlet.java
+++ example/src/servlet/HiddenServlet.java
@@ -2,8 +2,6 @@ package servlet;
 
 import java.io.IOException;
 import java.io.PrintWriter;
-import java.text.SimpleDateFormat;
-import java.util.Date;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -11,20 +9,18 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-@WebServlet("/SampleServlet")
-public class SampleServlet extends HttpServlet {
+@WebServlet("/HiddenServlet")
+public class HiddenServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		String luck = "超スッキリ";
-		Date date = new Date();
-		SimpleDateFormat sdf = new SimpleDateFormat("MM月dd日");
-		String today = sdf.format(date);
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		request.setCharacterEncoding("UTF-8");
+		String id = request.getParameter("id");
 		
 		response.setContentType("text/html; charset=UTF-8");
 		PrintWriter out = response.getWriter();
 		out.println("<html><body>");
-		out.println("<p>" + today + ":" + luck + "<body>");
+		out.println("id:" + id);
 		out.println("</body></html>");
 	}
 
---
+++ example/src/test/RegularExpressionSample.java
@@ -0,0 +1,30 @@
+package test;
+
+import java.util.regex.Pattern;
+
+public class RegularExpressionSample {
+
+	public static void main(String[] args) {
+		String str = "java";
+		
+		if (str.matches("^[0-9A-Za-z]{4}$")) {
+			System.out.println("一致しました");
+		} else {
+			System.out.println("一致していません");
+		}
+		
+	}
+	
+	public static void hozon() {
+		String str = "java";
+		
+		Pattern pattern 
+		  = Pattern.compile("^[0-9A-Za-z]{4}$");
+		if (pattern.matcher(str).matches()) {
+			System.out.println("一致しました");
+		} else {
+			System.out.println("一致していません");
+		}
+	}
+
+}
