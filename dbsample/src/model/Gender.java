ab025c65b74d8456194fb65e6b4c63759382b547
SeiichiN <billie175@gmail.com>
2023-01-06 16:30:08 +0900

    dbsample added .

---
+++ dbsample/src/model/Gender.java
@@ -0,0 +1,20 @@
+package model;
+
+import java.io.Serializable;
+
+public class Gender implements Serializable {
+	private String id;
+	private String name;
+	
+	public Gender () {}
+	public Gender(String id, String name) {
+		this.id = id;
+		this.name = name;
+	}
+	public String getId() {
+		return id;
+	}
+	public String getName() {
+		return name;
+	}
+}
