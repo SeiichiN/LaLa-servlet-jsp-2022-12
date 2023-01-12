b08f91367775d6ac76f52d7e49f5605cac27bf7e
SeiichiN <billie175@gmail.com>
2023-01-12 14:07:15 +0900

    ParamCheck 途中

---
+++ dbsample/src/model/MyError.java
@@ -0,0 +1,24 @@
+package model;
+
+import java.io.Serializable;
+
+public class MyError implements Serializable {
+	private String key;
+	private String msg;
+	
+	public MyError() {}
+	public MyError(String key, String msg) {
+		this.key = key;
+		this.msg = msg;
+	}
+	
+	public String toString() {
+		return key + ":" + msg;
+	}
+	public String getKey() {
+		return key;
+	}
+	public String getMsg() {
+		return msg;
+	}
+}
--- dbsample/src/servlet/create/CreateConfirmServlet.java
+++ dbsample/src/servlet/create/CreateConfirmServlet.java
@@ -1,6 +1,8 @@
 package servlet.create;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -9,7 +11,9 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import model.Employee;
+import model.MyError;
 import util.MyTool;
+import util.ParamCheck;
 
 @WebServlet("/createConfirm")
 public class CreateConfirmServlet extends HttpServlet {
@@ -20,6 +24,20 @@ public class CreateConfirmServlet extends HttpServlet {
 		// Employeeビーンズにまとめる
 		MyTool tool = new MyTool();
 		Employee emp = tool.getEmpByParam(request);
-	}	// empビーンズを使って、入力チェック。
+		// empビーンズを使って、入力チェック。
+		List<MyError> errorList = new ArrayList<>();
+		ParamCheck paramCheck = new ParamCheck();
+		paramCheck.validate(emp, errorList);
+		
+		request.setAttribute("emp", emp);
+		String url = "";
+		if (errorList.size() > 0) {
+			request.setAttribute("errorList", errorList);
+			url = "/WEB-INF/jsp/create/createEmp.jsp";
+		} else {
+			url = "/WEB-INF/jsp/create/createConfirm.jsp";
+		}
+		request.getRequestDispatcher(url).forward(request, response);
+	}	
 
 }
